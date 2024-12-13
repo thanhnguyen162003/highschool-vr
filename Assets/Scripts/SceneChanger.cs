17f85c95df8227e9fe051d1da5006c3251dd1d51
SHL <lehoangson2259@gmail.com>
2024-12-13 21:26:24 +0700

    new main menu

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneChanger : MonoBehaviour
 {
-    public void ChangeScene(string sceneName)
+    public string sceneToLoad;  // Scene name to load
+
+    // This method will be called when the button is clicked
+    public void LoadScene()
     {
-        SceneManager.LoadScene(sceneName);
+        SceneManager.LoadScene(sceneToLoad);
     }
 }
